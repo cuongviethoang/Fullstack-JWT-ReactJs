cb59985566258ac0489558b53b76503f8eb89068
cuongviethoang <hoangvietcuong2k2@gmail.com>
2024-02-16 10:29:10 +0700

    Cai dat react-router-dom va bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from "./logo.svg";
 import "./App.scss";
-
+import Nav from "./components/Navigation/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Notfound from "./components/NotFound/Notfound";
+import Login from "./components/Login/Login";
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <div className="app-container">
+                {/* <Nav /> */}
+                <Routes>
+                    <Route path="/login" element={<Login />}></Route>
+
+                    <Route path="/about">About</Route>
+                    <Route path="/news">News</Route>
+                    <Route path="/contact">Contact</Route>
+                    <Route path="/">Home</Route>
+                    <Route path="/*" element={<Notfound />}></Route>
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
